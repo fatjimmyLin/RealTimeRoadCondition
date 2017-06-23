b060f42a256664877177ec5209bb2cae7df835b2
wyc <914505102@qq.com>
2017-06-23 20:11:17 +0800

    ads

--- app/src/main/java/com/example/fatjimmy/realtimeroadcondition/MapsActivity.java
+++ app/src/main/java/com/example/fatjimmy/realtimeroadcondition/MapsActivity.java
@@ -22,11 +22,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-
     }
 
-//fatjimmy
-    //very fat
     /**
      * Manipulates the map once available.
      * This callback is triggered when the map is ready to be used.
