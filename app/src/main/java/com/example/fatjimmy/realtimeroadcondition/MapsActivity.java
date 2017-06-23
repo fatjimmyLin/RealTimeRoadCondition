be8b8087340a18e96730b630509e1453e876452c
fatjimmyLin <lht_1994@163.com>
2017-06-23 18:17:48 +0800

    commit1

--- app/src/main/java/com/example/fatjimmy/realtimeroadcondition/MapsActivity.java
+++ app/src/main/java/com/example/fatjimmy/realtimeroadcondition/MapsActivity.java
@@ -22,7 +22,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
-        int a = 7;
+        int a = 8;
         int b = 6;
     }
 
