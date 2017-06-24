c3bcd97d5cab62d9b1ca38103f216aacb7cf06fb
wyc <914505102@qq.com>
2017-06-24 11:52:41 +0800

    hongkong location

--- app/src/main/java/com/example/fatjimmy/realtimeroadcondition/MapsActivity.java
+++ app/src/main/java/com/example/fatjimmy/realtimeroadcondition/MapsActivity.java
@@ -38,8 +38,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         mMap = googleMap;
 
         // Add a marker in Sydney and move the camera
-        LatLng sydney = new LatLng(-34, 151);
-        mMap.addMarker(new MarkerOptions().position(sydney).title("Marker in Sydney"));
-        mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney));
+        LatLng hongkong = new LatLng(22.27, 114.2);
+        mMap.addMarker(new MarkerOptions().position(hongkong).title("Marker in Hong Kong"));
+        mMap.moveCamera(CameraUpdateFactory.newLatLng(hongkong));
     }
 }
